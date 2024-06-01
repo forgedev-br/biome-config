815cf125bdd3708fca493ee121dec54a4c1dc93d
Adeonir Kohl <adeonir@gmail.com>
2024-05-31 23:30:19 -0300

    refactor: remove base file

---
+++ .eslintrc.js
@@ -0,0 +1,130 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: [
+    'standard',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'prettier',
+  ],
+  env: {
+    es2022: true,
+    node: true,
+    browser: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  globals: {
+    React: true,
+    JSX: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['unicorn', 'import', 'simple-import-sort', 'prettier', 'react-refresh', 'react', 'react-hooks', 'jsx-a11y'],
+  rules: {
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        disallowTypeAnnotations: true,
+        fixStyle: 'separate-type-imports',
+        prefer: 'type-imports',
+      },
+    ],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        format: ['PascalCase'],
+        selector: ['typeLike', 'enumMember'],
+      },
+      {
+        custom: {
+          match: false,
+          regex: '^I[A-Z]|^(Interface|Props|State)$',
+        },
+        format: ['PascalCase'],
+        selector: 'interface',
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'error',
+    'import/default': 'off',
+    'import/namespace': 'off',
+    'import/newline-after-import': 'error',
+    'import/no-absolute-path': 'error',
+    'import/no-default-export': 'error',
+    'import/no-duplicates': 'error',
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-unresolved': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    'no-console': ['error', { allow: ['info', 'warn', 'error'] }],
+    'no-var': 'error',
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: 'if', next: '*' },
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: '*', next: 'function' },
+      { blankLine: 'always', prev: 'function', next: '*' },
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      {
+        blankLine: 'any',
+        prev: ['const', 'let', 'var'],
+        next: ['const', 'let', 'var'],
+      },
+      { blankLine: 'always', prev: 'directive', next: '*' },
+    ],
+    'prefer-const': 'warn',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'unicorn/filename-case': ['error', { case: 'kebabCase' }],
+    'unicorn/prefer-node-protocol': 'warn',
+    'jsx-a11y/alt-text': ['warn', { elements: ['img'], img: ['Image'] }],
+    'jsx-a11y/aria-props': 'warn',
+    'jsx-a11y/aria-proptypes': 'warn',
+    'jsx-a11y/aria-unsupported-elements': 'warn',
+    'jsx-a11y/role-has-required-aria-props': 'warn',
+    'jsx-a11y/role-supports-aria-props': 'warn',
+    'react/button-has-type': 'warn',
+    'react/function-component-definition': ['warn', { namedComponents: 'arrow-function' }],
+    'react/jsx-pascal-case': 'warn',
+    'react/no-unknown-property': 'error',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/self-closing-comp': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+  },
+  overrides: [
+    {
+      files: [
+        'vite.config.ts',
+        'src/**/*.stories.{ts,tsx}',
+        'tailwind.config.{js,ts,cjs}',
+        'postcss.config.{js,ts,cjs}',
+      ],
+      rules: {
+        'import/no-default-export': 'off',
+      },
+    },
+  ],
+}
